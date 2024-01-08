336f7487f7c10d837d7a616c32416625b934a189
Hector <141383937+hfelix3@users.noreply.github.com>
2024-01-07 22:15:11 -0700

    added thoght controllers and updated users controllers

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
-// Get all users
+//GET all users
 router.get('/', async (req, res) => {
     try {
       const UserData = await User.findAll();
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     }
   });
   
-// Get one User
+//GET a single user by its _id and populated thought and friend data
   router.get('/:id', async (req, res) => {
     try {
       const UserData = await Tag.findOne({ 
@@ -28,7 +28,21 @@ router.get('/', async (req, res) => {
     }
   });
 
-//update a user
+//POST a new user:
+router.post('/', async (req, res) => {
+    try {
+      const newUser = await User.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newUser);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//PUT to update a user by its _id
 router.put('/:id', async(req, res) => {
     const UserData = await User.update(
         req.body,
@@ -43,7 +57,7 @@ router.put('/:id', async(req, res) => {
     return res.json(UserData);
   });
 
-//Delete user
+//DELETE to remove user by its _id
 router.delete('/:id', async (req, res) => {
   try {
     const UserData = await User.destroy({
