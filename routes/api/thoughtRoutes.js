143947157ac99b7213d608218162e190a6a5f510
Hector <141383937+hfelix3@users.noreply.github.com>
2024-01-15 20:37:33 -0700

    added thought routes and removed api folder from controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtsController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/thoughts/:reactionId
+router.route('/:thoughtId/reactions/:reactionsId').delete(removeReaction);
+
+module.exports = router;
